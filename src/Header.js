344f88daf36fd727d1e3065e95dece57b7a1d484
SanjanaRajarajan <153099329+SanjanaRajarajan@users.noreply.github.com>
2024-11-19 19:06:29 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <nav>
         <ul>
           <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
+          <li><a href="#about me">About</a></li>
           <li><a href="#resume">Resume</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
